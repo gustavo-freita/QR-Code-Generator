a13b8e9fe4585dcf3b85dcced3ce24a557a26179
Gustavo Freitas <gustavofreitas_silva@hotmail.com>
2023-08-25 19:46:43 -0300

    Finished QR code generator project

--- assets/js/generateQR.js
+++ assets/js/generateQR.js
@@ -3,5 +3,13 @@ let imageQR = document.getElementById("imageQR");
 let textQR = document.getElementById("textQR");
 
 function generate() {
-    imageQR.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + textQR.value;
+    if (textQR.value.length > 0) {
+        imageQR.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + textQR.value;
+    }else {
+        textQR.classList.add('error');
+        setTimeout (()=> {
+            qrText.classList.add('error');
+        },1000);
+    }
+    imgQR.classList.add("show-img");
 }
